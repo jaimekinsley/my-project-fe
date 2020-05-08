a4ee963aa981fecc0a980bdae978e606ea5ee16f
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-08 15:50:07 -0700

    adds detail page

---
+++ src/DetailPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { getCloud } from './api-calls.js';
+
+export default class DetailPage extends Component {
+    state = { cloud: null }
+    componentDidMount = async () => {
+        const fetchedData = await getCloud(this.props.match.params.id);
+
+        this.setState({ cloud: fetchedData })
+    }
+
+    render() {
+        return (
+            <div>
+                Detail Page
+
+                <div>
+                {
+                    this.state.song
+                    ? JSON.stringify(this.state.cloud)
+                    : 'Loading........'
+                }
+                </div>
+            </div>
+        )
+    }
+}
